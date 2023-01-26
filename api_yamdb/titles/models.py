9e81e954944a411c72596dd5d80e3a67900bc24e
Алексей Акимов <akimov_alexey98@mail.ru>
2023-01-26 11:48:46 +0600

    update models

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -9,24 +9,13 @@ class Category(models.Model):
         return self.name
 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Title(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(blank=True)
     year = models.IntegerField()
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name='titles')
-    genre = models.ForeignKey(
-        Genre,
-        on_delete=models.CASCADE,
-        related_name='titles'
+        Category, on_delete=models.SET_NULL,
+        null=True, related_name='titles'
     )
 
     class Meta:
@@ -36,3 +25,38 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True)
+    titles = models.ManyToManyField(
+        Title,
+        through='GenreTitle',
+        related_name='genre',
+        verbose_name='жанр произведения',
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'genre'], name='GenreTitle'
+            )
+        ]
+
+    def __str__(self):
+        return f'Это произведение {self.title} жанра {self.genre}'
