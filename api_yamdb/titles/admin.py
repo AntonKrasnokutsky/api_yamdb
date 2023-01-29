c5604dab9e975da2a269d39b3505003c6aaef29e
Антон Краснокутский <anton.krasnokutsky@gmail.com>
2023-01-29 13:30:50 +0300

    Запуск тестов

--- api_yamdb/titles/admin.py
+++ api_yamdb/titles/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Title, Genre, GenreTitle, Category
+from .models import Title, Genre, Category
 
 admin.site.register(Title)
 admin.site.register(Genre)
-admin.site.register(GenreTitle)
 admin.site.register(Category)
