6ce263b10290e940cf06a58441b61bbe851de4b9
Алексей Акимов <akimov_alexey98@mail.ru>
2023-01-30 11:49:25 +0600

    db updated

--- api_yamdb/titles/management/commands/data_load.py
+++ api_yamdb/titles/management/commands/data_load.py
@@ -3,17 +3,19 @@ from django.core.management import BaseCommand, CommandError
 from django.db.models.base import ModelBase
 from titles.models import (
     Title, Category, Genre,
-    GenreTitle, Comment, Review, User
+    GenreTitle, Comment, Review
 )
+from users.models import User
+
 
 TABLES_DICT = {
+    User: 'users.csv',
     Category: 'category.csv',
     Genre: 'genre.csv',
     Title: 'titles.csv',
     GenreTitle: 'genre_title.csv',
     Review: 'review.csv',
     Comment: 'comments.csv',
-    User: 'users.csv'
 }
 
 
@@ -28,13 +30,24 @@ class Command(BaseCommand):
                     id=int(row['category'])
                 )
                 return row
+        elif cur_model is Review:
+            if 'author' in row.keys():
+                row['author'] = User.objects.get(
+                    id=int(row['author'])
+                )
+                return row
+        elif cur_model is Comment:
+            if 'author' in row.keys():
+                row['author'] = User.objects.get(
+                    id=int(row['author'])
+                )
+                return row
         return row
 
     def handle(self, *args, **options):
         for model, file_name in TABLES_DICT.items():
             if model.objects.exists():
-                #raise CommandError('Data exists in database!')
-                continue
+                raise CommandError('Data exists in database!')
             with open(
                     f'./static/data/{file_name}',
                     encoding='utf-8'
