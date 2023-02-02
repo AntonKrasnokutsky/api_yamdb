8af3ded5201d51891c4cd0b1536126048e493fc8
Алексей Акимов <akimov_alexey98@mail.ru>
2023-02-02 19:14:43 +0600

    updated models, serializers,urls (genre,title,category

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from .views import (
     ReviewViewSet, TittleViewSet, GenreViewSet, CategoryViewSet
 )
-from users.views import UserViewSet, user_get_token, user_signup
+
 
 router = routers.DefaultRouter()
 router.register(r'titles/(?P<title_id>\d+)/reviews',
@@ -13,10 +13,7 @@ router.register(r'titles/(?P<title_id>\d+)/reviews',
 router.register(r'titles', TittleViewSet, basename='Title')
 router.register(r'genres', GenreViewSet, basename='Genre')
 router.register(r'categories', CategoryViewSet, basename='Category')
-router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('auth/token/', user_get_token, name='get_token'),
-    path('auth/signup/', user_signup, name='signup'),
-    path('v1/', include(router.urls)),
+    path('api/v1/', include(router.urls)),
 ]
