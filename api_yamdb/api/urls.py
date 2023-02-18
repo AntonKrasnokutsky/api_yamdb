c5ccac46c86b23d697b358307dd7a37083bae3b8
Антон Краснокутский <anton.krasnokutsky@gmail.com>
2023-02-18 19:02:06 +0300

    Замечания ревью

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from django.urls import include, path
+
 from .views import (
     ReviewViewSet, TittleViewSet, GenreViewSet, CategoryViewSet,
     CommentViewSet,
@@ -7,18 +8,19 @@ from .views import (
 
 
 router = routers.DefaultRouter()
-router.register(r'titles/(?P<title_id>\d+)/reviews',
-                ReviewViewSet,
-                basename='reviews'
-                )
-router.register(r'titles/(?P<title_id>\d+)/reviews'
-                r'/(?P<review_id>\d+)/comments',
-                CommentViewSet,
-                basename='comments'
-                )
-router.register(r'titles', TittleViewSet, basename='Title')
-router.register(r'genres', GenreViewSet, basename='Genre')
-router.register(r'categories', CategoryViewSet, basename='Category')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router.register('titles', TittleViewSet, basename='Title')
+router.register('genres', GenreViewSet, basename='Genre')
+router.register('categories', CategoryViewSet, basename='Category')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
